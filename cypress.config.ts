5a8ac9da2de30be6b372e6a55ce95205ffc77472
Lucas Fidelis <lucasm241301@gmail.com>
2026-05-28 21:08:05 -0300

    chore: use env variables for baseUrl and apiUrl in Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.BASE_URL || "http://localhost:8080",
     supportFile: "cypress/support/e2e.ts",
-    specPattern: "cypress/e2e/**/*.cy.ts",
+  },
+  expose: {
+    apiUrl: process.env.API_URL || "http://localhost:3000",
   },
 });
